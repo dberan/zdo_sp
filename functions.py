aa09b2d9393ee3bf7ba26f30bed8b3c1320a5c82
HynekMarek <hy.marek@gmail.com>
2018-05-04 18:10:11 +0200

    Doplneni funkci pro detekci zmen a vypis BB

--- functions.py
+++ functions.py
@@ -1,2 +1,52 @@
+import numpy as np
+import skimage
+
 def testFunc(string):
     print(string)
+    
+    
+    
+def hist(vstup):
+    r = vstup.shape
+    
+    h = np.zeros(256)
+    
+    for i in range(0,r[0]):
+        for j in range(0,r[1]):
+            h[int(vstup[i,j])] += 1
+    return h
+
+
+
+def detektor_zmen_ve_snimku(vstup1,vstup2,px_vel):
+        
+    r = vstup1.shape
+    rozl = [int(r[0]/px_vel),int(r[1]/px_vel)]
+    
+    zmenovy_rastr = np.zeros(rozl)
+    for i in range(0,rozl[0]):
+        for j in range(0,rozl[1]):
+            seg1 = vstup1[i*px_vel:(i+1)*px_vel,j*px_vel:(j+1)*px_vel]
+            seg2 = vstup2[i*px_vel:(i+1)*px_vel,j*px_vel:(j+1)*px_vel]
+            
+            h1 = hist(seg1)
+            h2 = hist(seg2)
+            zmenovy_rastr[i,j] = sum(abs(h1-h2))
+            
+    return zmenovy_rastr
+
+
+
+
+def bboxy(inp):
+    
+    imlabel1 = skimage.measure.label(inp, background=0)
+    pocet_objektu = np.max(imlabel1)
+    bboxy = np.zeros((pocet_objektu,4)) 
+    
+    for i in range(1,pocet_objektu+1):
+        objekt = (imlabel1==i).astype(int)
+        dat = skimage.measure.regionprops(objekt)
+        bboxy[i-1,:] = dat[0].bbox
+    
+    return bboxy
--- run.py
+++ run.py
@@ -1,4 +1,49 @@
 import functions
+import glob
+import os.path as op
+import imageio
+import skimage
+import skimage.io
+from skimage.feature import local_binary_pattern
+import matplotlib.pyplot as plt
 
 functions.testFunc('test')
 
+
+
+
+    #prozatim jsem doplnil spusteni funkci ze sveho testovaciho souboru.
+datapath = "../videaSP"
+fnvideos = glob.glob(op.join(datapath, "*.AVI"))
+
+
+allframes = None
+
+ivideo = 9
+step = 1
+
+fn = fnvideos[ivideo]
+print("video:", fn)
+
+vid = imageio.get_reader(fn)
+
+frame_size = vid.get_meta_data()["size"]
+frame_number = vid.get_length()
+
+
+#allframes = np.zeros([int(frame_number / step), int(frame_size[1]), int(frame_size[0]), 3], dtype=np.uint8)
+
+
+poz = skimage.io.imread('..\pozadi\V__00014_xvid_poz.jpg')
+akt = skimage.color.rgb2gray(vid.get_data(30))
+
+
+m = 'default'
+poz_lbp = local_binary_pattern(poz,8,2,method=m)
+akt_lbp = local_binary_pattern(akt,8,2,method=m)
+
+#parametr px_vel zadavat jenom v hodnotach spolecneho delitele 720, 1280 (rozliseni obrazu)
+test = functions.detektor_zmen_ve_snimku(poz_lbp,akt_lbp,20)
+
+plt.imshow(test)
+plt.show()
